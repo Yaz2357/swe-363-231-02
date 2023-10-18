8a58a6806cc7b0a6c09ca6c6933200f923fc53bc
Yaz2357 <yifdu5577@gmail.com>
2023-10-18 11:56:13 +0300

    JS_2_EXERCISE_2_B

--- SPECIAL EXERCISES/script.js
+++ SPECIAL EXERCISES/script.js
@@ -6,34 +6,87 @@ let board = [
 ];
 
 function makeMove(row, col) {
-    if (board[row][col] === "") {
-      document.getElementById("board").children[row * 3 + col].textContent = currentPlayer;
-      board[row][col] = currentPlayer;
-      if (checkWin()) {
-        showMessage(currentPlayer + " wins!");
-      } else if (checkDraw()) {
-        showMessage("It's a draw!");
-      } else {
-        currentPlayer = currentPlayer === "X" ? "O" : "X";
-      }
+  if (board[row][col] === "") {
+    document.getElementById("board").children[row * 3 + col].textContent = currentPlayer;
+    board[row][col] = currentPlayer;
+    if (checkWin()) {
+      showMessage(currentPlayer + " wins!");
+    } else if (checkDraw()) {
+      showMessage("It's a draw!");
+    } else {
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
     }
   }
-  
-  function showMessage(message) {
-    const messageElement = document.createElement("div");
-    messageElement.textContent = message;
-    messageElement.classList.add("message");
-  
-    document.body.appendChild(messageElement);
-  
-    setTimeout(function () {
-      messageElement.classList.add("fade");
-      setTimeout(function () {
-        messageElement.remove();
-        resetBoard();
-      }, 500);
-    }, 1000);
+}
+
+function handleKey(event) {
+  const key = event.key;
+  let row, col;
+
+  switch (key) {
+    case "1":
+      row = 0;
+      col = 0;
+      break;
+    case "2":
+      row = 0;
+      col = 1;
+      break;
+    case "3":
+      row = 0;
+      col = 2;
+      break;
+    case "4":
+      row = 1;
+      col = 0;
+      break;
+    case "5":
+      row = 1;
+      col = 1;
+      break;
+    case "6":
+      row = 1;
+      col = 2;
+      break;
+    case "7":
+      row = 2;
+      col = 0;
+      break;
+    case "8":
+      row = 2;
+      col = 1;
+      break;
+    case "9":
+      row = 2;
+      col = 2;
+      break;
+    case "0":
+      resetBoard();
+      break;
+    default:
+      return; // Ignore other keys
   }
+
+  makeMove(row, col);
+}
+
+document.addEventListener("keydown", handleKey);
+
+function showMessage(message) {
+  const messageElement = document.createElement("div");
+  messageElement.textContent = message;
+  messageElement.classList.add("message");
+
+  document.body.appendChild(messageElement);
+
+  setTimeout(function () {
+    messageElement.classList.add("fade");
+    setTimeout(function () {
+      messageElement.remove();
+      resetBoard();
+    }, 500);
+  }, 1000);
+}
 
 function checkWin() {
   for (let i = 0; i < 3; i++) {
@@ -49,7 +102,7 @@ function checkWin() {
       board[0][i] === board[1][i] &&
       board[1][i] === board[2][i]
     ) {
-      return true; 
+      return true;
     }
   }
   if (
@@ -57,14 +110,14 @@ function checkWin() {
     board[0][0] === board[1][1] &&
     board[1][1] === board[2][2]
   ) {
-    return true; 
+    return true;
   }
   if (
     board[0][2] !== "" &&
     board[0][2] === board[1][1] &&
     board[1][1] === board[2][0]
   ) {
-    return true; 
+    return true;
   }
   return false;
 }
@@ -73,11 +126,11 @@ function checkDraw() {
   for (let row = 0; row < 3; row++) {
     for (let col = 0; col < 3; col++) {
       if (board[row][col] === "") {
-        return false; 
+        return false;
       }
     }
   }
-  return true; 
+  return true;
 }
 
 function resetBoard() {
