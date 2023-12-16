0f0078f2f99a22a0ad9f4350c8396098d915c5eb
Yaz2357 <yifdu5577@gmail.com>
2023-12-16 16:26:05 +0300

    JS_3_EXERCISE_(ALT/ 1&2)

--- script.js
+++ script.js
@@ -27,3 +27,37 @@ for(let i=0; i<books.length;i++){
     html += `<tr> <td>${books[i].Title}</td> <td>${books[i].Author}</td> <td>${books[i].Genre}</td> <td>${books[i].Price}</td> </tr>`;
 }
 document.getElementById("top-selling-table").innerHTML= html;
+
+const canvas = document.getElementById('weather-canvas');
+const ctx = canvas.getContext('2d');
+
+function drawWeather(weatherData) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw weather information on the canvas
+  // Example: Display temperature
+  const temperature = weatherData.main.temp;
+  ctx.font = '20px Arial';
+  ctx.fillStyle = 'white';
+  ctx.fillText(`The Khobar city Temperature is ${temperature}°C`, 10, 30);
+}
+
+function fetchWeatherData() {
+  // Make an API call to retrieve weather data
+  // Replace 'YOUR_API_KEY' with your actual API key
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=Khobar&appid=63693afd04404d7615059bce6d9818d8&units=metric')
+    .then(response => response.json())
+    .then(weatherData => {
+      // Call the drawWeather function to render the data on the canvas
+      drawWeather(weatherData);
+    })
+    .catch(error => {
+      console.log('Error fetching weather data:', error);
+    });
+}
+
+// Call fetchWeatherData initially
+fetchWeatherData();
+
+// Call fetchWeatherData every 10 seconds
+setInterval(fetchWeatherData, 10000);
